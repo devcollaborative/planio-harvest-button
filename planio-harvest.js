3b1c65d5bfa4f207b39a4ca1b1d37dccaaa3fde3
Heather Brokmeier <heather@devcollaborative.com>
2024-01-05 12:51:26 -0600

    Use harvest iframe widget

--- planio-harvest.js
+++ planio-harvest.js
@@ -1,15 +1,34 @@
 window.addEventListener("DOMContentLoaded", function() {
+  // Get Harvest info for iframe
+  const appName = "Planio";
+
   // Get Planio issue ID and title
-  let issueID = document.querySelector('.icon-edit').getAttribute('href').split("/")[2];
-  let issueTitle = document.querySelector('.subject h3').innerText;
+  const issueID = document.querySelector('.icon-edit').getAttribute('href').split("/")[2];
+  const issueTitle = document.querySelector('.subject h3').innerText;
+  const issueName = `#${issueID} ${issueTitle}`;
+  const permalink = `${window.location.origin}/issues/${issueID}`;
 
-  // Add Harvest Button placeholder
-  let copyButton = document.querySelector('.icon-copy');
-  copyButton.insertAdjacentHTML('afterend', `<a class="harvest-timer" style="min-height: 16px; margin-left: 3px;" data-item='{"id": ${issueID}, "name": "#${issueID} ${issueTitle}"}'></div>`);
+  // Construct iframe with Harvest params
+  const iframeSrc = `https://platform.harvestapp.com/platform/timer?app_name=${encodeURIComponent(appName)}&permalink=${encodeURIComponent(permalink)}&external_item_id=${encodeURIComponent(issueID)}&external_item_name=${encodeURIComponent(issueName)}`;
+  const iframe = document.createElement('iframe');
 
-  // Inject Harvest Button script
-  let harvestButtonScript = document.createElement("script");
-  harvestButtonScript.setAttribute("src", "https://platform.harvestapp.com/assets/platform.js");
-  harvestButtonScript.setAttribute("data-platform-config", `{ "applicationName": "Planio", "permalink": "${window.location.origin}/issues/%ITEM_ID%" }`);
-  document.body.appendChild(harvestButtonScript);
+  // Get sidebar element where iframe will be added
+  document.getElementById("sidebar").prepend(iframe);
+
+  // Insert iframe, set initial styles, and trigger resize event for frame
+  iframe.src = iframeSrc;
+  iframe.setAttribute("style","border: 0; width: 100%; height: 400px; padding-top: 1rem;");
+  window.dispatchEvent(new Event('frame:resize'));
+
+  // Resize iframe
+  window.addEventListener("message", function (event) {
+    console.log(event);
+    if (event.origin != "https://platform.harvestapp.com") {
+      return;
+    }
+
+    if (event.data.type == "frame:resize") {
+      iframe.style.height = event.data.value + "px";
+    }
+  });
 });
